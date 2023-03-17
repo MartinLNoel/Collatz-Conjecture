fc9bb598fe8eaff59962373eee70bb7bf60e3620
Martin <Martin@liquidicestudios.com>
2023-03-17 16:45:59 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    public bool IsEven(int number)
+    {
+        if (number % 2 == 0)
+            return true;
+        else
+            return false;
+    }
+
+    public void Calculation(int number)
+    {
+        if (IsEven(number) == true)
+            number /= 2;
+        else
+            number = number * 3 + 1;
+    }
+}
