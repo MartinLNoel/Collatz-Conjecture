0f250427e4ba6796beab93f7190ae04026ee01a1
Martin <Martin@liquidicestudios.com>
2023-03-20 13:31:21 +0100

    Made the comparision not include the given number since it needs to be below 100 as in except for 100.

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 
 class Program
 {
-    //A function that determines if the number given is odd or even adn return true if its even.
+    //A function that determines if the number given is odd or even and return true if its even.
     public bool IsEven(int number)
     {
         if (number % 2 == 0)
@@ -15,19 +15,27 @@ class Program
     {
        return (pastNumber > currentNumber) ? pastNumber : currentNumber;
     }
+    //Function that outputs the console.writeLine of all the neseccary information
+    public void Text(int pastNumber, int count)
+    {
+        Console.WriteLine("Biggest Number: " + pastNumber);
+        Console.WriteLine("Number of steps: " + count);
+    }
 
     //The "main" function that calls the IsEven function and depending on the returned bool does divide by 2 or multiple by 3 plus 1.
-    public int Calculation(int number)
+    public void Calculation(int number)
     {
         //Variables used to save the numbers which will be used to compare later
-        int pastNumber = number;
-        int currentNumber = number;
+        int pastNumber = 0;
 
         //The variable that is returned at the end and counts how many times the given number went through the process.
         int count = 1;
         //An if statement that prevents the given number to be 0 or less
         if (number <= 0)
-            return 0;
+        {
+            Console.WriteLine("Please enter a number higher than 0.");
+            Environment.Exit(0);
+        }
         //A while loop that causes the main calculation to be on repeat until the given number is 1
         while (number != 1)
         {
@@ -40,20 +48,19 @@ class Program
                 //if odd then the number is multiplied by 3 plus 1
                 number = number * 3 + 1;
             //Saving the new calculated number into currentNumber
-            currentNumber = number;
+            int currentNumber = number;
             //Calling the function that will compare the two numbers and output the biggest number
             pastNumber = CompareNumber(currentNumber, pastNumber);
         }
-        //Output the biggest number
-        Console.WriteLine("Biggest Number: " + pastNumber);
-        //Return the count calculated throught the process
-        return count;
+        //Cal the function to print all information once exited the while loop
+        Text(pastNumber, count);
     }
+
 
     public static void Main(string[] args)
     {
         Program p = new Program();
-        Console.WriteLine("Number of steps: " + p.Calculation(7));
+        p.Calculation(100);
     }
 
 }
